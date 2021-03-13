a38d1395549ec813c58062faaef61ab5e34fe3cd
Mino <paffrath@yangnet.de>
2021-03-13 18:01:42 +0100

    Security fix

--- assets/php/setUserSettingsA.php
+++ assets/php/setUserSettingsA.php
@@ -3,7 +3,7 @@ $conn = mysqli_connect("127.0.0.1","swpuser","swpuser","swp");
 if(mysqli_connect_errno()){
     die();
 }
-$sql = "SELECT Admin FROM User WHERE UserID = ".$_SESSION['user-id'];
+$sql = "SELECT Admin FROM User WHERE Admin = 1 AND UserID = ".$_SESSION['user-id'];
 $res = $conn->query($sql);
 if($res->num_rows != 1){
     die("Permission denied");
