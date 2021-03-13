af6ade8ec40d4424349c15c3785fa1aa352ff8f2
Mino <paffrath@yangnet.de>
2021-03-13 14:04:11 +0100

    added specific id functionality

--- assets/php/getUsers.php
+++ assets/php/getUsers.php
@@ -3,7 +3,17 @@ $conn = mysqli_connect("127.0.0.1","swpuser","swpuser","swp");
 if(mysqli_connect_errno()){
     die();
 }
-$res= mysqli_query($conn,"SELECT UserID,Vorname,Nachname,Username,Email,Kontostand FROM User WHERE Admin = '0';");
+$sql = "SELECT Admin FROM User WHERE UserID = ". "1";//$_SESSION['user-id'];
+$res = $conn->query($sql);
+if($res->num_rows != 1){
+    http_response_code(403);
+    die();
+}
+if(isset($_GET['id'])){
+    $res= $conn->query("SELECT UserID,Vorname,Nachname,Username,Email,Kontostand FROM User WHERE Admin = '0' AND UserID = ".$_GET['id'].";");
+}else{
+    $res= $conn->query("SELECT UserID,Vorname,Nachname,Username,Email,Kontostand FROM User WHERE Admin = '0';");
+}
 $rows = array();
 while ($r = mysqli_fetch_assoc($res)){
     $rows[] = $r;
