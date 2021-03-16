861642011110e70fa44c1d05bccefb4eb68524cc
Mino5531 <paffrath@yangnet.de>
2021-03-16 15:19:18 +0100

    small bug fix

--- assets/php/inc.php
+++ assets/php/inc.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "127.0.0.1";
-$username = "swp";
-$password = "swp";
+$username = "swpuser";
+$password = "swpuser";
 $dbname = "swp";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
