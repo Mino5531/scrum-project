a93d962601edc12f2c2e4a81af1ff86d87de005b
JakobRaupach <jakob.raupach@gmail.com>
2021-03-17 11:13:07 +0100

    added connection to games on index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
 $("#btn-blackjack").click(function(){
 	window.location.href = "blackjack.html"
 })
+$("#btn-lotto").click(function(){
+	window.location.href = "lotto.html"
+})
+$("#btn-slots").click(function() {
+	window.location.href = "slots.html"
+})
