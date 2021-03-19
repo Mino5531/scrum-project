c73f9f6457cdb44a61d96a6aa810ecf0d31e9bb0
Mino <paffrath@yangnet.de>
2021-03-19 19:11:57 +0100

    hotfix

--- assets/php/getUserData.php
+++ assets/php/getUserData.php
@@ -4,7 +4,7 @@ $conn = mysqli_connect("127.0.0.1","swpuser","swpuser","swp");
 if(mysqli_connect_errno()){
     die();
 }
-$res = $conn->query("SELECT * FROM User Where UserID = ".$_SESSION['user-id'].";");
+$res = $conn->query("SELECT UserID,Vorname,Nachname,Username,Kontostand,Admin,Email,Passwort,Addresse,Stadt,Land,Gesperrt FROM User Where UserID = ".$_SESSION['user-id'].";");
 $rows = array();
 while ($r = mysqli_fetch_assoc($res)){
     $rows[] = $r;
