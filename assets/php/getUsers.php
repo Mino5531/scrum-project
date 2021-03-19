45b854b25dd7f156fc5e3b06a6ee408dc04b67d4
Mino <paffrath@yangnet.de>
2021-03-19 19:31:31 +0100

    hotfix 3

--- assets/php/getUsers.php
+++ assets/php/getUsers.php
@@ -26,8 +26,10 @@ if(isset($_GET['id'])){
             }
 
             $array = ["img"=>$image];
-
+            header('Content-Type: application/json');
             echo json_encode($array);
+            http_response_code(200);
+            die();
         }
     }else{
         $res= $conn->query("SELECT Vorname,Nachname,Username,Email,Addresse,Stadt,Land,Gesperrt FROM User WHERE Admin = '0' AND UserID = ".$_GET['id'].";");
